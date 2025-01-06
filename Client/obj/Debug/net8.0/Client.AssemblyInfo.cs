ac555094a81c652a1f57ab45aea2571503097b0e
MarlineIOS <blanxxtygadress@gmail.com>
2025-01-07 00:27:30 +0300

    Added MudBlazor

--- Client/obj/Debug/net8.0/Client.AssemblyInfo.cs
+++ Client/obj/Debug/net8.0/Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a240e2b257d28347ba46705782ab4dbfabd526c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca4b38afb9585e6c53d7fc10a2d12db5137049d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
