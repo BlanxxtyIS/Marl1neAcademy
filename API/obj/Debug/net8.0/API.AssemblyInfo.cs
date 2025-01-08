c2829364489ca2af700ae4717b1d67a17f92ea80
MarlineIOS <blanxxtygadress@gmail.com>
2025-01-08 20:26:23 +0300

    Initial create

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac555094a81c652a1f57ab45aea2571503097b0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8670a52cba3d8d99505c543ef9fe881f19e35b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Models/obj/Debug/net8.0/Models.AssemblyInfo.cs
+++ Models/obj/Debug/net8.0/Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca4b38afb9585e6c53d7fc10a2d12db5137049d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8670a52cba3d8d99505c543ef9fe881f19e35b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
